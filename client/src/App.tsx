ce00ea7d9e3b1aaf6ed6af9159ffd424a2c50a6a
Ida Lam <idaslam@outlook.com>
2020-09-17 21:58:57 +0200

    added route to login page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,20 @@
+import 'normalize.css'
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+// import logo from './logo.svg'
+// import './App.css'
+import Login from './views/login/login'
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <Router>
+            <Switch>
+                <Route path="/">
+                    <Login />
+                </Route>
+            </Switch>
+        </Router>
     )
 }
 

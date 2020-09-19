fe734c48edcd5c39e9277073e9bf70e71641b779
Ida Lam <idaslam@outlook.com>
2020-09-19 19:58:38 +0200

    added a theme for material design

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,18 +3,33 @@ import React from 'react'
 // import logo from './logo.svg'
 // import './App.css'
 import Login from './views/login/login'
+import {createMuiTheme, ThemeProvider} from "@material-ui/core"
+import indigo from '@material-ui/core/colors/indigo'
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
+const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: indigo[900],
+      },
+      secondary: {
+        main: indigo[700],
+      },
+    },
+});
+
 function App() {
     return (
-        <Router>
-            <Switch>
-                <Route path="/">
-                    <Login />
-                </Route>
-            </Switch>
-        </Router>
+        <ThemeProvider theme={theme}>
+            <Router>
+                <Switch>
+                    <Route path="/">
+                        <Login />
+                    </Route>
+                </Switch>
+            </Router>
+        </ThemeProvider>
     )
 }
 

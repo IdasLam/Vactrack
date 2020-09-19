42226357774b993545d59f487ab13ee4a2b08e2f
Ida Lam <idaslam@outlook.com>
2020-09-19 19:59:02 +0200

    validation for email and password

---
+++ client/src/services/validation.ts
@@ -0,0 +1,15 @@
+import Joi from "joi"
+
+export const email = (inputEmail: string) => {
+    const schema = Joi.string().email({ tlds: {allow: false} });
+    const { error } = schema.validate(inputEmail);
+
+    return error === undefined
+}
+
+export const password = (inputPassword: string) => {
+    const schema = Joi.string().min(1).required()
+    const { error } = schema.validate(inputPassword);
+
+    return error === undefined
+}

a172e8f72e4846ec8280d13c2e4c6667408fb135
Ida Lam <idaslam@outlook.com>
2020-09-23 16:17:43 +0200

    added post route to create user in databse

--- server/src/routes/family.ts
+++ server/src/routes/family.ts
@@ -5,16 +5,41 @@ import admin from 'firebase-admin'
 const db = admin.firestore()
 const router = Express.Router()
 
+// Checks if user is in the database
 router.post(
     '/account',
     asyncFunction(async (req, res) => {
         const { uid } = req.body
+
         try {
             // const doc = await db.collection('family').get()
             await db.collection('family').doc(uid).get()
             res.sendStatus(200)
         } catch (error) {
             res.sendStatus(404)
+        }
+    }),
+)
+
+// Create user in databse
+router.post(
+    '/account/create',
+    asyncFunction(async (req, res) => {
+        const { uid, name } = req.body
+
+        const docRef = db.collection('family').doc(uid)
+
+        try {
+            await docRef.set({
+                [name]: {
+                    status: 'user',
+                    vaccines: [],
+                    active_vaccines: [],
+                },
+            })
+            res.sendStatus(200)
+        } catch (error) {
+            res.sendStatus(400)
         }
     }),
 )

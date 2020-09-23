54da9a91e962c2e461992a39661dee355e22188f
Ida Lam <idaslam@outlook.com>
2020-09-23 19:05:07 +0200

    added only logo component

--- client/src/components/logo/logo.tsx
+++ client/src/components/logo/logo.tsx
@@ -5,7 +5,6 @@ const SloganLogo: FunctionComponent = () => {
     return (
         <div className="logo-container">
             <img className="logo-container__img" src="vactrack.svg" alt="" />
-            <p className="logo-container__slogan">Keeps track of you vaccinations</p>
         </div>
     )
 }

e8de47d8b77be48f90d6454c68d22a8c45a22a8f
Ida Lam <idaslam@outlook.com>
2020-09-23 16:22:09 +0200

    WIP header

---
+++ client/src/components/m-header/m-header.tsx
@@ -0,0 +1,12 @@
+import React, { FunctionComponent } from 'react'
+import './header.scss'
+
+const MHeader: FunctionComponent = () => {
+    return (
+        <header>
+            <h1>asdsaasd</h1>
+        </header>
+    )
+}
+
+export default MHeader

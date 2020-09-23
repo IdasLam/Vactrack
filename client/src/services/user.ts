4fa68eedd20d995514ac123c1e8317e3862ec258
Ida Lam <idaslam@outlook.com>
2020-09-23 12:14:31 +0200

    added clientside login and out, authentication

--- client/src/services/user.ts
+++ client/src/services/user.ts
@@ -1,13 +1,73 @@
-import Axios from "axios"
+import * as firebase from 'firebase/app'
+import 'firebase/auth'
+import { stringify } from 'querystring'
+import * as fetch from '../models/family/family'
 
-type Credantials2 = {
-    email: string,
-    password: string
+type SignIn = () => void
+type SignOut = () => void
+type OAuth = () => void
+
+const provider = new firebase.auth.GoogleAuthProvider()
+
+export const signIn: SignIn = () => {
+    firebase.auth().signInWithRedirect(provider)
 }
 
-export const login = async (credantials: Credantials2) => {
-    const response = await Axios.post('/api/login', credantials)
+export const signOut: SignOut = () => {
+    firebase
+        .auth()
+        .signOut()
+        .then(function () {
+            // Sign-out successful.
+        })
+        .catch(function (error) {
+            // An error happened.
+        })
+}
 
-    console.log(response);
-    
+export const Oauth: OAuth = () => {
+    firebase
+        .auth()
+        .getRedirectResult()
+        .then(async function (result) {
+            // console.log(result)
+            // if (result.credential) {
+            //     // This gives you a Google Access Token. You can use it to access the Google API.
+            //     const token = result.credential.accessToken
+            //     // ...
+            // }
+            // // The signed-in user info.
+            const user = result.user
+
+            if (user !== null) {
+                console.log(user.uid)
+
+                const status = await fetch.login(user.uid)
+
+                console.log(status.status)
+            }
+
+            // firebase.auth().onAuthStateChanged((user) => {
+            //     if (user) {
+            //         // User logged in already or has just logged in.
+            //         console.log(user.uid)
+            //     } else {
+            //         // User not logged in or has just logged out.
+            //     }
+            // })
+        })
+        .catch(function (error) {
+            console.log(error)
+
+            // Handle Errors here.
+            const errorCode = error.code
+            const errorMessage = error.message
+            // The email of the user's account used.
+            // const email = error.email
+            // The firebase.auth.AuthCredential type that was used.
+            // const credential = error.credential
+            // ...
+
+            console.log(errorMessage)
+        })
 }

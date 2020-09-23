b0ee97adb4825ed31d3e5d027cd1dd0482164df3
Ida Lam <idaslam@outlook.com>
2020-09-23 18:52:53 +0200

    rewrote oaut to login instead

--- client/src/services/user/user.ts
+++ client/src/services/user/user.ts
@@ -24,51 +24,10 @@ export const signOut = () => {
 
 export const Oauth = () => {
     return firebase.auth().getRedirectResult()
-    // .then(async function (result) {
-    //     // console.log(result)
-    //     // if (result.credential) {
-    //     //     // This gives you a Google Access Token. You can use it to access the Google API.
-    //     //     const token = result.credential.accessToken
-    //     //     // ...
-    //     // }
-    //     // // The signed-in user info.
-    //     const user = result.user
+}
 
-    //     if (user !== null) {
-    //         // console.log(user.uid)
-    //         const response = await fetch.login(user.uid)
+export const isLoggedIn = () => {
+    // console.log(firebase.auth().currentUser)
 
-    //         if (response.status === 200) {
-    //             window.location.assign(window.location.origin + '/home')
-    //         }
-    //     }
-
-    //     // firebase.auth().onAuthStateChanged((user) => {
-    //     //     if (user) {
-    //     //         // User logged in already or has just logged in.
-    //     //         console.log(user.uid)
-    //     //     } else {
-    //     //         // User not logged in or has just logged out.
-    //     //     }
-    //     // })
-    // })
-    // .catch(function (error) {
-    //     // console.log(error)
-
-    //     // Handle Errors here.
-    //     // const errorCode = error.code
-    //     const errorMessage = error.message
-    //     // The email of the user's account used.
-    //     // const email = error.email
-    //     // The firebase.auth.AuthCredential type that was used.
-    //     // const credential = error.credential
-    //     // ...
-
-    //     // console.log(errorMessage)
-
-    //     if (errorMessage === 'Request failed with status code 404') {
-    //         console.log('not in databse')
-    //         createUser()
-    //     }
-    // })
+    return firebase.auth().currentUser !== null
 }

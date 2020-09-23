e5e5af347d63478f41f75b78a67a3202d5e1ee57
Ida Lam <idaslam@outlook.com>
2020-09-23 19:59:43 +0200

    added the burgermenu and logo to header

--- client/src/components/m-header/m-header.tsx
+++ client/src/components/m-header/m-header.tsx
@@ -1,10 +1,27 @@
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, useState } from 'react'
+import Logo from '../logo/logo'
+import Menu from './menu'
+// import { MenuIcon } from '@material-ui/icons'
+import MenuIcon from '@material-ui/icons/Menu'
+import Button from '@material-ui/core/Button'
 import './header.scss'
 
 const MHeader: FunctionComponent = () => {
+    const [menu, setMenu] = useState(false)
+
     return (
         <header>
-            <h1>asdsaasd</h1>
+            <div className="menu-header">
+                <Button
+                    onClick={() => {
+                        setMenu(true)
+                    }}
+                >
+                    <MenuIcon color="primary" fontSize="large" />
+                </Button>
+                <Logo />
+            </div>
+            <div>{menu ? <Menu onClose={() => setMenu(false)} /> : null}</div>
         </header>
     )
 }

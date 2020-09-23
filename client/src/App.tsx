3efbc914b1d98ebeb8cbfb562529e975271fd347
Ida Lam <idaslam@outlook.com>
2020-09-23 12:10:37 +0200

    init firebase app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,21 +3,37 @@ import React from 'react'
 // import logo from './logo.svg'
 // import './App.css'
 import Login from './views/login/login'
-import {createMuiTheme, ThemeProvider} from "@material-ui/core"
+import { createMuiTheme, ThemeProvider } from '@material-ui/core'
 import indigo from '@material-ui/core/colors/indigo'
-
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import * as firebase from 'firebase/app'
+import 'firebase/analytics'
+import 'firebase/database'
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyBOuPwXoezhpGhnHBFyl9AouCzODbzMWQA',
+    authDomain: 'vactrack-87132.firebaseapp.com',
+    databaseURL: 'https://vactrack-87132.firebaseio.com',
+    projectId: 'vactrack-87132',
+    storageBucket: 'vactrack-87132.appspot.com',
+    messagingSenderId: '91574914055',
+    appId: '1:91574914055:web:ff382a849dce483795eea2',
+    measurementId: 'G-RVBYD3F8WN',
+}
+
+firebase.initializeApp(firebaseConfig)
+firebase.analytics()
 
 const theme = createMuiTheme({
     palette: {
-      primary: {
-        main: indigo[900],
-      },
-      secondary: {
-        main: indigo[700],
-      },
+        primary: {
+            main: indigo[900],
+        },
+        secondary: {
+            main: indigo[700],
+        },
     },
-});
+})
 
 function App() {
     return (

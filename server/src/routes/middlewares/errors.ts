756496c7a8c33c58b0ffb15f2d7a297063fab19c
Ida Lam <idaslam@outlook.com>
2020-09-23 12:12:25 +0200

    added firebase error handler

--- server/src/routes/middlewares/errors.ts
+++ server/src/routes/middlewares/errors.ts
@@ -1,15 +1,21 @@
 import express from 'express'
-import { ValidationError } from "joi"
-import {Response, Request, NextFunction} from 'express'
+import { ValidationError } from 'joi'
+import { Response, Request, NextFunction } from 'express'
+import util from 'util'
 
+type Middleware = (error: any, req: Request, res: Response, next: NextFunction) => void
 
-export const JoiError = (error: any, req: Request, res: Response, next: NextFunction) => {
+export const JoiError: Middleware = (error, req, res, next) => {
     if (!(error instanceof ValidationError)) {
         return next(error)
     }
 
     res.status(400).json({
-        type: "JoiError",
-        message: error.details[0].message
+        type: 'JoiError',
+        message: error.details[0].message,
     })
+}
+
+export const firebaseError: Middleware = (error, req, res, next) => {
+    console.log('firebase cuck', util.inspect(error, { showHidden: true, depth: null }))
 }

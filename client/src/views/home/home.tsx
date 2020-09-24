afd6789f470213b2a9f926e8d74ed416317005af
Ida Lam <idaslam@outlook.com>
2020-09-24 18:29:45 +0200

    fetched data from firebase

--- client/src/views/home/home.tsx
+++ client/src/views/home/home.tsx
@@ -1,24 +1,37 @@
-import React, { FunctionComponent, useEffect } from 'react'
-import * as firebase from 'firebase/app'
+import React, { FunctionComponent, useEffect, useState } from 'react'
 import Layout from '../../components/layout/layout'
 import { useHistory } from 'react-router-dom'
 import * as user from '../../services/user/user'
+import * as fetch from '../../services/family/family'
+
+import firebase from 'firebase/app'
+import { useDocumentData } from 'react-firebase-hooks/firestore'
+
+const firestore = firebase.firestore()
 
 const Home: FunctionComponent = () => {
     const history = useHistory()
+    const [uid, setUid] = useState<string>()
+    const doc = firestore.doc(`family/${uid}`)
+    const [value] = useDocumentData(doc)
 
     useEffect(() => {
         const isLoggedIn = user.isLoggedIn()
-        console.log('mounting')
 
         if (!isLoggedIn) {
             history.replace('/')
+        } else {
+            setUid(user.getUid())
         }
     }, [])
 
+    if (value) {
+        console.log(value)
+    }
+
     return (
         <Layout>
-            <h1></h1>
+            <h1>ello</h1>
         </Layout>
     )
 }

24f4b3dcb324f31bd5d49126dba32f4987920ac4
Ida Lam <idaslam@outlook.com>
2020-09-25 23:57:35 +0200

    added hook that gets the width of window

--- client/src/services/hooks/hooks.tsx
+++ client/src/services/hooks/hooks.tsx
@@ -1,5 +1,35 @@
+import { useState, useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
+
+// type Window = {
+//     width: undefined | number
+// }
 
 export const useQuery = () => {
     return new URLSearchParams(useLocation().search)
+}
+
+export const useWindowSize = () => {
+    // Initialize state with undefined width/height so server and client renders match
+    // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+    const [windowSize, setWindowSize] = useState<number | undefined>(undefined)
+
+    useEffect(() => {
+        // Handler to call on window resize
+        const handleResize = () => {
+            // Set window width/height to state
+            setWindowSize(window.innerWidth)
+        }
+
+        // Add event listener
+        window.addEventListener('resize', handleResize)
+
+        // Call handler right away so state gets updated with initial window size
+        handleResize()
+
+        // Remove event listener on cleanup
+        return () => window.removeEventListener('resize', handleResize)
+    }, []) // Empty array ensures that effect is only run on mount
+
+    return windowSize
 }

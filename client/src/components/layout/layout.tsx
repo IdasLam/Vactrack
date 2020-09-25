152179a83444badc61356e4ebd51375e4f60d905
Ida Lam <idaslam@outlook.com>
2020-09-25 23:57:55 +0200

    when width is above 950 disable mobile header

--- client/src/components/layout/layout.tsx
+++ client/src/components/layout/layout.tsx
@@ -1,17 +1,28 @@
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, useEffect, useState } from 'react'
 import MHeader from '../m-header/m-header'
+import DHeader from '../d-header/d-header'
 import './layout.scss'
 import MiddeButton from '../button/button'
+import { useWindowSize } from '../../services/hooks/hooks'
 
 const Layout: FunctionComponent = (props) => {
-    return (
-        <div>
-            <MHeader />
-            <main>{props.children}</main>
-            <MiddeButton />
-            {/* footer? */}
-        </div>
-    )
+    const size = useWindowSize()
+
+    if (size) {
+        return (
+            <React.Fragment>
+                <div className="wrapper-container">
+                    {size < 950 ? <MHeader /> : null}
+                    <DHeader />
+                    <main>{props.children}</main>
+                    {/* footer? */}
+                </div>
+                <MiddeButton />
+            </React.Fragment>
+        )
+    }
+
+    return null
 }
 
 export default Layout

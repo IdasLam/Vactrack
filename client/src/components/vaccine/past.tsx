9922a9c4a57d78da300c2c4183575c21d8108ae8
Ida Lam <idaslam@outlook.com>
2020-09-25 22:30:11 +0200

    component that adds cards for past vaccinations

---
+++ client/src/components/vaccine/past.tsx
@@ -0,0 +1,35 @@
+import React, { FunctionComponent } from 'react'
+import { AllTypesOfVaccines } from '../../models/family'
+import dayjs from 'dayjs'
+import { convertDate } from '../../helpers/helpers'
+
+type Cards = {
+    vaccines: AllTypesOfVaccines[]
+}
+
+const PastCards: FunctionComponent<Cards> = (props) => {
+    const { vaccines } = props
+
+    const generateCards = () => {
+        const cards = vaccines.map((vaccine) => {
+            const { name, revaccination, date } = vaccine
+
+            return (
+                // key={name + vaccine.name + convertDate(vaccine.revaccination)
+                <div className="vaccine-card" key={name + convertDate(date)}>
+                    <p className="vaccine-name">{name}</p>
+                    {revaccination ? <span>{convertDate(revaccination)}</span> : null}
+                    <p>Vaccination date: {convertDate(date)}</p>
+                </div>
+            )
+        })
+
+        console.log(cards)
+
+        return cards
+    }
+
+    return <div>{generateCards()}</div>
+}
+
+export default PastCards

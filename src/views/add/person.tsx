cafc8aac06edbe9a7f595589a7a4fcd1f2e1b39c
Ida Lam <idaslam@outlook.com>
2020-09-28 14:03:29 +0200

    added form and fetch template

--- src/views/add/person.tsx
+++ src/views/add/person.tsx
@@ -1,11 +1,54 @@
 import React, { FunctionComponent, useEffect, useState } from 'react'
+import { FormControl, TextField } from '@material-ui/core'
 import Layout from '../../components/layout/layout'
+import firebase from 'firebase/app'
+import * as user from '../../services/user/user'
+import { Family } from '../../models/family'
+import Loader from '../../components/loading/loading'
+
+import { useDocumentData } from 'react-firebase-hooks/firestore'
+
+import * as validation from '../../services/validation/person'
+
+const firestore = firebase.firestore()
 
 const AddPerson: FunctionComponent = () => {
+    const [errorName, setErrorName] = useState(false)
+    const [uid, setUid] = useState<string>()
+
+    useEffect(() => {
+        setUid(user.getUid())
+    }, [setUid])
+
+    const doc = firestore.doc(`family/${uid}`)
+    const [value, loading] = useDocumentData<Family>(doc)
+
+    useEffect(() => {
+        if (value) {
+            console.log(value)
+        }
+    }, [value])
+    // const submit = (event) => {
+
+    // }
+
+    if (loading) {
+        return <Loader />
+    }
+
     return (
         <Layout>
             <section className="main-section-container">
                 <h1>hello</h1>
+                <FormControl>
+                    <TextField
+                        onChange={(event) => {
+                            console.log(event.target.value)
+                        }}
+                        label="text"
+                        error={errorName}
+                    />
+                </FormControl>
             </section>
         </Layout>
     )

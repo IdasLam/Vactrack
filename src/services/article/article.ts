36aa1fb3b519149c7ff6275025f613dd635c58ba
Ida Lam <idaslam@outlook.com>
2020-09-28 11:07:08 +0200

    get two articles function,

--- src/services/article/article.ts
+++ src/services/article/article.ts
@@ -4,5 +4,22 @@
 import { Article } from '../../models/article'
 
 export const getOneArticle = (articles: Article[]) => {
-    console.log(articles)
+    const articlesCount = Object.keys(articles).length
+
+    const articleRandomIndex = parseInt(Object.keys(articles)[Math.floor(Math.random() * articlesCount)])
+
+    return articles[articleRandomIndex]
+}
+
+export const getTwoArticles = (articles: Article[]) => {
+    const firstArticle = getOneArticle(articles)
+    let secondArticle = getOneArticle(articles)
+
+    console.log(firstArticle !== secondArticle)
+
+    while (firstArticle.title === secondArticle.title) {
+        secondArticle = getOneArticle(articles)
+    }
+
+    return [firstArticle, secondArticle]
 }

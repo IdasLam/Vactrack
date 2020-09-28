60dafa57c4f461586a11882cd14df568ab63891e
Ida Lam <idaslam@outlook.com>
2020-09-28 13:00:31 +0200

    init new views for edit person and vaccine

---
+++ src/views/add/person.tsx
@@ -0,0 +1,14 @@
+import React, { FunctionComponent, useEffect, useState } from 'react'
+import Layout from '../../components/layout/layout'
+
+const AddPerson: FunctionComponent = () => {
+    return (
+        <Layout>
+            <section className="main-section-container">
+                <h1>hello</h1>
+            </section>
+        </Layout>
+    )
+}
+
+export default AddPerson
---
+++ src/views/add/vaccine/vaccine.tsx
@@ -0,0 +1,14 @@
+import React, { FunctionComponent } from 'react'
+import Layout from '../../../components/layout/layout'
+
+const AddVaccine: FunctionComponent = () => {
+    return (
+        <Layout>
+            <section className="main-section-container">
+                <h1>hello</h1>
+            </section>
+        </Layout>
+    )
+}
+
+export default AddVaccine

87d37cbd3ff749914ad40a50ecbb2a21c36a6926
Ida Lam <idaslam@outlook.com>
2020-09-28 12:27:15 +0200

    added component for the feature cards

---
+++ src/components/article/article.tsx
@@ -0,0 +1,31 @@
+import React, { FunctionComponent } from 'react'
+import * as ArticleTypes from '../../models/article'
+import { Button } from '@material-ui/core'
+import './article.scss'
+
+type ArticleComponent = {
+    articles: ArticleTypes.Article[] | undefined
+}
+
+const Article: FunctionComponent<ArticleComponent> = (props) => {
+    const { articles } = props
+
+    const generateCards = () => {
+        return articles?.map((article) => {
+            const { link, text, title } = article
+
+            return (
+                <Button className="article-container" key={link + text + title}>
+                    <a target="__BLANK" href={link}>
+                        <p className="title">{title}</p>
+                        <p>{text}</p>
+                    </a>
+                </Button>
+            )
+        })
+    }
+
+    return <div className="featuring-article-container">{generateCards()}</div>
+}
+
+export default Article

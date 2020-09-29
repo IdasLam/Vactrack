09f4786c3997351039b86edb344eebcfd956297a
Ida Lam <idaslam@outlook.com>
2020-09-29 15:22:52 +0200

    added correct form

--- src/views/add/vaccine.tsx
+++ src/views/add/vaccine.tsx
@@ -2,33 +2,34 @@ import React, { FunctionComponent, useEffect, useState } from 'react'
 import Layout from '../../components/layout/layout'
 import firebase from 'firebase/app'
 import * as user from '../../services/user/user'
-import * as family from '../../services/family/family'
+// import * as family from '../../services/family/family'
 import { Family, Name } from '../../models/family'
 import * as fetch from '../../services/family/family'
 import Loader from '../../components/loading/loading'
 import { useDocumentData } from 'react-firebase-hooks/firestore'
-import * as validation from '../../services/validation/person'
-import { FormControl, TextField, Button, MenuItem, InputLabel } from '@material-ui/core'
+// import * as validation from '../../services/validation/person'
+import { FormControl, TextField, FormControlLabel, Switch, MenuItem, FormGroup } from '@material-ui/core'
 import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers'
 import dayjs from 'dayjs'
 import dayjsUtils from '@date-io/dayjs'
-import { useHistory } from 'react-router-dom'
+// import { useHistory } from 'react-router-dom'
 import MiddleButtonSubmit from '../../components/button/sumbit'
-import './person.scss'
-import { useQuery } from '../../services/hooks/hooks'
+import './form.scss'
+// import { useQuery } from '../../services/hooks/hooks'
 
 const firestore = firebase.firestore()
 // con history
 
 const AddVaccine: FunctionComponent = () => {
-    const history = useHistory()
-    const query = useQuery()
+    // const history = useHistory()
+    // const query = useQuery()
 
-    const [errorName, setErrorName] = useState(false)
-    // const [status, setStatus] = useState<string>('')
+    const [inputVaccineName, setInputVaccineName] = useState<string>('')
     const [inputName, setInputName] = useState<string>('')
     const [date, setDate] = useState<any>(dayjs().format('YYYY-MM-DD'))
-    // const [valid, setValid] = useState<boolean>(false)
+    const [revaccinate, setRevaccinate] = useState<boolean>(false)
+    const [remindNumber, setRemindNumber] = useState<string>('0')
+    const [remindTime, setRemindTime] = useState<string>('')
 
     const [nameList, setNameList] = useState<Name[]>()
 
@@ -66,6 +67,8 @@ const AddVaccine: FunctionComponent = () => {
     const submit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault()
 
+        console.log(inputVaccineName, inputName, date, revaccinate, remindNumber, remindTime)
+
         // if (valid && uid && value) {
         //     const dateformat = date ? firebase.firestore.Timestamp.fromDate(new Date(date)) : ''
         //     family.addPerson(uid, status, name, dateformat)
@@ -83,8 +86,6 @@ const AddVaccine: FunctionComponent = () => {
         })
     }
 
-    console.log(nameList)
-
     return (
         <Layout>
             <section className="main-section-container person">
@@ -92,7 +93,16 @@ const AddVaccine: FunctionComponent = () => {
                 <form onSubmit={submit}>
                     <FormControl>
                         <TextField
-                            label="Name"
+                            value={inputVaccineName}
+                            onChange={(event) => {
+                                // check if already in list
+                                setInputVaccineName(event.target.value)
+                            }}
+                            label="Vaccine name"
+                            required
+                        />
+                        <TextField
+                            label="Person name"
                             value={inputName}
                             select
                             onChange={(event) => {
@@ -102,27 +112,13 @@ const AddVaccine: FunctionComponent = () => {
                         >
                             {addOptions()}
                         </TextField>
-                        {/* <TextField
-                            label="Status"
-                            value={'snopp'}
-                            select
-                            onChange={(event) => {
-                                console.log(event.target.value)
-                                // setStatus(event.target.value)
-                            }}
-                            required
-                        >
-                            <MenuItem value="Custodian/Parent">Custodian/Parent</MenuItem>
-                            <MenuItem value="Child">Child</MenuItem>
-                            <MenuItem value="Pet">Pet</MenuItem>
-                        </TextField> */}
                         <MuiPickersUtilsProvider utils={dayjsUtils}>
                             <KeyboardDatePicker
                                 disableToolbar
                                 variant="inline"
                                 format="YYYY-MM-DD"
                                 margin="normal"
-                                label="Birthday"
+                                label="Vaccination date"
                                 value={date}
                                 onChange={(inputDate) => setDate(inputDate?.format('YYYY-MM-DD'))}
                                 KeyboardButtonProps={{
@@ -130,6 +126,48 @@ const AddVaccine: FunctionComponent = () => {
                                 }}
                             />
                         </MuiPickersUtilsProvider>
+                        <FormGroup aria-label="position" row>
+                            <FormControlLabel
+                                label="Revaccination"
+                                labelPlacement="start"
+                                control={
+                                    <Switch
+                                        checked={revaccinate}
+                                        onChange={() => {
+                                            setRevaccinate(!revaccinate)
+                                        }}
+                                    />
+                                }
+                            />
+                        </FormGroup>
+                        {revaccinate ? (
+                            <div>
+                                <TextField
+                                    label="Remind in"
+                                    type="number"
+                                    value={remindNumber}
+                                    onChange={(event) => {
+                                        setRemindNumber(event.target.value)
+                                    }}
+                                    InputLabelProps={{
+                                        shrink: true,
+                                    }}
+                                    required
+                                />
+                                <TextField
+                                    label="Month/Year"
+                                    value={remindTime}
+                                    select
+                                    onChange={(event) => {
+                                        setRemindTime(event.target.value)
+                                    }}
+                                    required
+                                >
+                                    <MenuItem value="month">month</MenuItem>
+                                    <MenuItem value="year">year(s)</MenuItem>
+                                </TextField>
+                            </div>
+                        ) : null}
                     </FormControl>
                     <MiddleButtonSubmit valid={true} />
                 </form>

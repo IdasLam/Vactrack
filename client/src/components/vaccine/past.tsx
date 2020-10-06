c8b1011d8803fe4f37d1df1b72fbceb2f31e7822
Ida Lam <idaslam@outlook.com>
2020-10-06 14:35:31 +0200

    added link to edit

--- client/src/components/vaccine/past.tsx
+++ client/src/components/vaccine/past.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent } from 'react'
 import { AllTypesOfVaccines } from '../../models/family'
 import { convertDate } from '../../helpers/helpers'
+import { Link } from 'react-router-dom'
 
 type Cards = {
     vaccines: AllTypesOfVaccines[]
@@ -10,16 +11,18 @@ const PastCards: FunctionComponent<Cards> = (props) => {
     const { vaccines } = props
 
     const generateCards = () => {
-        const cards = vaccines.map((vaccine, index) => {
-            const { name, revaccination, date } = vaccine
+        const cards = vaccines.map((vaccine) => {
+            const { name, revaccination, date, id } = vaccine
 
             return (
                 // key={name + vaccine.name + convertDate(vaccine.revaccination)
-                <div className="vaccine-card" key={name + convertDate(date) + index}>
-                    <p className="vaccine-name">{name}</p>
-                    {revaccination ? <span>Due {convertDate(revaccination)}</span> : null}
-                    <p>Vaccinated: {convertDate(date)}</p>
-                </div>
+                <Link to={'/edit/vaccine?id=' + id + '&name=' + name} key={name + id}>
+                    <div className="vaccine-card">
+                        <p className="vaccine-name">{name}</p>
+                        {revaccination ? <span>Due {convertDate(revaccination)}</span> : null}
+                        <p>Vaccinated: {convertDate(date)}</p>
+                    </div>
+                </Link>
             )
         })
 

b11e46f114aa1e5705d5f159a449a359e5cdd753
Ida Lam <idaslam@outlook.com>
2020-10-09 11:54:26 +0200

    removed unused file

--- client/src/services/api/api.ts
+++
@@ -1,7 +0,0 @@
-const isProd = window.location.hostname !== 'localhost'
-const host = window.location.hostname
-const port = isProd ? '' : ':3001'
-const subdomain = isProd ? 'api.' : ''
-const protocal = isProd ? 'https:' : 'http:'
-
-export const url = `${protocal}//${subdomain}${host}${port}`
